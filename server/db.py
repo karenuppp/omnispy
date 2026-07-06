8897708da5c146f1c6a9c1f3a94cfb3a4c29ddf8
Karen <karen@gleam.local>
2026-07-06 09:43:40 +0800

    fix: add check_same_thread=False for SQLite thread safety in FastAPI

--- server/db.py
+++ server/db.py
@@ -13,7 +13,7 @@ class Database:
         self._conn: sqlite3.Connection | None = None
 
     def connect(self):
-        self._conn = sqlite3.connect(str(self._path))
+        self._conn = sqlite3.connect(str(self._path), check_same_thread=False)
         self._conn.row_factory = sqlite3.Row
         self._conn.execute("PRAGMA journal_mode=WAL")
         self._conn.execute("PRAGMA foreign_keys=ON")
